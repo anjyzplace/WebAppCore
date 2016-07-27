b564441371e8b3e5c0fddd90e57750add763f50a
anjyzplace <anjola.awofisoye@gmail.com>
2016-07-27 08:38:43 +0100

    major commit

---
+++ src/WebAppCoreUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAppCoreUnitTests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5bf2066-c6d0-4685-a47f-888f214b235a")]
---
+++ src/WebAppCoreUnitTests/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebAppCoreUnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+        [Fact]
+        public void UsefulTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void MoreTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+        [Fact]
+        public void AnotherTest()
+        {
+            Assert.Equal(5, Add(2, 3));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
